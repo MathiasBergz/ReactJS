2716ac964899d3a57f7c41b13203ba66388c3c44
mathias <mathias15052004@gmail.com>
2023-05-02 18:23:39 -0300

    Created ButtonText component

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,26 +1,32 @@
-import { Container, Links } from './styles.js'
+import { Container, Links, Content } from './styles.js'
 
 import { Button } from '../../components/button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
+import { Tags } from '../../components/Tags'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
   return(
     <Container> 
       <Header />
-      <Section title="Links uteis">
-        <Links>
-          <li><a href="#">Link 1</a></li>          
-          <li><a href="#">Link 1</a></li>          
-        </Links>
-      </Section>
-      <Section title="Links uteis">
-        <Links>
-          <li><a href="#">Link 1</a></li>          
-          <li><a href="#">Link 1</a></li>          
-        </Links>
-      </Section>
-      <Button title="Voltar"/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota"/>
+          
+          <Section title="Links uteis">
+            <Links>
+              <li><a href="#">Link 1</a></li>          
+              <li><a href="#">Link 1</a></li>          
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tags title="express"/>
+            <Tags title="nodejs"/>
+          </Section>
+          <Button title="Voltar"/>
+        </Content>
+      </main>
     </Container>
   )
 }
--- rocketnotes/src/pages/Details/styles.js
+++ rocketnotes/src/pages/Details/styles.js
@@ -5,10 +5,16 @@ export const Container = styled.div`
     height: 100vh;
 
     display: grid;
-    grid-template-rows: 105px auto;
+    grid-template-rows: 10.5rem auto;
     grid-template-areas: 
     "header"
     "content"
+
+    > main {
+        grid-area: content;
+        overflow-y: scroll;
+        padding: 6.5rem 0;
+    }
 `;
 
 export const Links = styled.ul`
@@ -24,3 +30,6 @@ export const Links = styled.ul`
         margin-top: 1.2rem;
     }
 ` 
+export const Content = styled.div`
+    max-width: 55rem
+`
