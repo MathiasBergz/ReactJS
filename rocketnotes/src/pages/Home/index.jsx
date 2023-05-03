2b415148ab115c26644278d02d7286dd82b5643e
mathias <mathias15052004@gmail.com>
2023-05-03 19:32:15 -0300

    Created input component

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNotes } from './styles'
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
+import { Input } from '../../components/Input'
 
 export function Home() {
     return(
@@ -16,7 +17,7 @@ export function Home() {
                 <li><ButtonText title="Node"/></li>                
             </Menu>
             <Search>
-
+                <Input placeholder="Pesquisar pelo título"/>
             </Search>
             <Content>
 
