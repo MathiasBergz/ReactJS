967455164d6f71e6629b5a91027c4304168fbecb
mathias <mathias15052004@gmail.com>
2023-05-05 16:21:16 -0300

    Created SignIn page and assets folder

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -5,12 +5,13 @@ import theme from './styles/theme'
 import GlobalStyle from './styles/global'
 import { Details } from './pages/Details'
 import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ rocketnotes/src/pages/SignIn/styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+
+    display: flex;
+    align-items: stretch;
+`
+
+export const Form = styled.form`
+    padding: 0 13.6rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    text-align: center;
+
+    > h1 {
+        font-size: 4.8rem;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+
+    > p {
+        font-size: 1.4rem;
+        color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    > h2 {
+        font-size: 2.4rem;
+        color: ${({ theme }) => theme.COLORS.WHITE};
+        margin: 4.8rem 0;
+    }
+
+    > a {
+        margin-top: 12.4rem;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+`
