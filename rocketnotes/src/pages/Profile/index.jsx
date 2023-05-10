e09986d4a4cd3da73fdb47c4341cc08595f4b61e
mathias <mathias15052004@gmail.com>
2023-05-10 17:29:20 -0300

    Created Profile page

--- rocketnotes/src/pages/Profile/index.jsx
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -1,9 +1,59 @@
-import { Container } from "./styles";
+import { Container, Form, Avatar } from "./styles";
+import { FiArrowLeft, FiMail, FiLock, FiUser, FiCamera } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/button";
 
 export function Profile() {
     return(
         <Container>
-            
+            <header>
+                <a href="/">
+                    <FiArrowLeft/>
+                </a>
+            </header>
+
+            <Form>
+                <Avatar>
+                    <img 
+                        src="https://github.com/MathiasBergz.png" 
+                        alt="Foto de perfil" 
+                    />
+
+                    <label htmlFor="avatar">
+                        <FiCamera />
+
+                        <input
+                        id="avatar" 
+                        type="file" 
+                        />
+                    </label>
+                </Avatar>
+                <Input 
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
+
+                <Input 
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input 
+                    placeholder="Senha atual"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Input 
+                    placeholder="Nova senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Salvar" />
+            </Form>
         </Container>
     )
 }
--- rocketnotes/src/pages/Profile/styles.js
+++ rocketnotes/src/pages/Profile/styles.js
@@ -2,4 +2,72 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
+
+    > header {
+        width: 100%;
+        height: 14.4rem;
+
+        background: ${({ theme }) => theme.COLORS.BACKGROUND_900 };
+
+        display: flex;
+        align-items: center;
+
+        padding: 0 14.4rem;
+
+        svg {
+            color: ${({ theme }) => theme.COLORS.GRAY_100};
+            font-size: 2.4rem;
+        }
+    }
+`
+
+export const Form = styled.form`
+    max-width: 34rem;
+    margin: 3rem auto;
+
+    > div:nth-child(4) {
+        margin-top: 2.4rem;
+    }
+`
+
+export const Avatar = styled.div`
+    position: relative;
+    margin: -12.6rem auto 6.4rem;
+
+    height: 18.6rem;
+    width: 18.6rem;
+    
+    > img {
+        border-radius: 50%;
+        height: 18.6rem;
+        width: 18.6rem;
+    }
+
+    > label {
+        width: 4.8rem;
+        height: 4.8rem;
+
+        background: ${({ theme }) => theme.COLORS.ORANGE};
+        border-radius: 50%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        right: .4rem;
+        bottom: .4rem;
+
+        cursor: pointer;
+
+        input {
+            display: none;
+        }
+
+        svg {
+            width: 2rem;
+            height: 2rem;
+            color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+        }
+    }
 `
