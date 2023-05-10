7f7b61679c3dc66255595f00a114123af39fec8f
mathias <mathias15052004@gmail.com>
2023-05-10 15:47:29 -0300

    Removed the variable FiLogIn and added FiUser

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -6,12 +6,13 @@ import GlobalStyle from './styles/global'
 import { Details } from './pages/Details'
 import { Home } from './pages/Home'
 import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- rocketnotes/src/pages/SignUp/index.jsx
+++ rocketnotes/src/pages/SignUp/index.jsx
@@ -1,17 +1,24 @@
-import { Container, Form, Background} from "./styles";
-import { FiMail, FiLock } from 'react-icons/fi'
+import { Container, Form, Background } from "./styles";
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/button'
 
-export function SignIn () {
+export function SignUp () {
     return(
         <Container>
+            <Background />
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-                <h2>Faça seu login</h2>
+                <h2>Crie sua conta</h2>
+
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
 
                 <Input
                     placeholder="E-mail"
@@ -25,13 +32,12 @@ export function SignIn () {
                     icon={FiLock}
                 />
 
-                <Button title="Entrar"/>
+                <Button title="Cadastrar"/>
 
                 <a href="#">
-                    Criar conta
+                    Voltar para o login
                 </a>
             </Form>
-            <Background />
         </Container>
     )
 }
