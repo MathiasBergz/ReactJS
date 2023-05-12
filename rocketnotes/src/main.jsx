7bd47f0ab4f09b2b9219476460a2c52bcfb4c915
mathias <mathias15052004@gmail.com>
2023-05-12 00:57:59 -0300

    Created routes to the website

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyle from './styles/global'
-import { Details } from './pages/Details'
-import { Home } from './pages/Home'
-import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
-import { Profile } from './pages/Profile'
-import { New } from './pages/New'
+
+import { Routes } from './routes'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <New />
+      <Routes/>
     </ThemeProvider>
   </React.StrictMode>,
 )
