3fe544aa8a9fabe75ed920d4a80e546108ee5fea
mathias <mathias15052004@gmail.com>
2023-05-12 00:31:39 -0300

    Created NewNotes page and installed react-router-dom

---
+++ rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,48 @@
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { TextArea } from '../../components/TextArea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/button'
+
+import { Container, Form } from './styles'
+
+export function New() {
+    return (
+        <Container>
+            <Header/>
+            
+            <main>
+                <Form>
+                    <header>
+                        <h2>Criar nota</h2>
+                        <a href="/">voltar</a>
+                    </header>
+
+                    <Input placeholder="Título"/>
+                    <TextArea placeholder="Observações"/>
+
+                    <Section title="Links uteis" >
+                        <NoteItem value="htpps//app.rocketseat.com.br"/>
+                        <NoteItem 
+                        isNew
+                        placeholder="Novo link"
+                        />
+                    </Section>
+
+                    <Section title="Marcadores" >
+                        <div className='tags'>
+                            <NoteItem value="React"/>
+                            <NoteItem 
+                            isNew
+                            placeholder="Novo marcador"
+                            />
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar" />
+                </Form>
+            </main>
+        </Container>
+    )
+}
