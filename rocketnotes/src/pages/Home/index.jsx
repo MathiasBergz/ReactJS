16f9cbe3ebb216198e8b6b40412adaaf18dd6c9e
mathias <mathias15052004@gmail.com>
2023-06-12 17:53:59 -0300

    Create function to fecth notes according to tags selected

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -12,8 +12,13 @@ import { Note } from '../../components/Note'
 export function Home() {
     const [tags, setTags] = useState([])
     const [tagsSelected, setTagsSelected] = useState([])
+    const [search, setSearch] = useState("")
+    const [notes, setNotes] = useState([])
 
     function handleTagSelected(tagName) {
+        if(tagName === "all") {
+            return setTagsSelected([])
+        }
         const alreadySelected = tagsSelected.includes(tagName)
 
         if(alreadySelected) {
@@ -32,6 +37,15 @@ export function Home() {
 
         fetchTags()
     }, [])
+
+    useEffect(() => {
+        async function fetchNotes() {
+            const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`)
+            setNotes(response.data)
+        }
+
+        fetchNotes()
+    }, [tagsSelected, search])
 
     return(
         <Container>
@@ -59,21 +73,21 @@ export function Home() {
                 }
             </Menu>
             <Search>
-                <Input placeholder="Pesquisar pelo título"/>
+                <Input 
+                placeholder="Pesquisar pelo título"
+                onChange={() => setSearch(e.target.value)}
+                />
             </Search>
             <Content>
                 <Section title="Minhas notas">
-                    <Note data={{
-                        title: 'React',
-                        tags: [
-                            {
-                                id: '1', name: 'react'
-                            },
-                            {
-                                id: '2', name: 'rocketseat'
-                            }
-                    ]
-                    }}/>
+                    {
+                        notes.map(note => (
+                            <Note
+                            key={String(note.id)} 
+                            data={note}
+                            />
+                        ))
+                    }
                 </Section>
             </Content>
             <NewNotes to="/new">
